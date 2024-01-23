32abe4eaf70f0b573ed2623a394f40b2111504e3
Mdavid508 <81154696+Mdavid508@users.noreply.github.com>
2024-01-23 19:29:28 +0300

    creation of Dismantlers and Collectors Folders and Logic

---
+++ lib/views/dismantlers/dismantlers_home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DismantlersHomePage extends StatelessWidget {
+  const DismantlersHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
