9e5aabe905e8b4eeb4c94df3d8e8b892045a9f39
Mdavid508 <81154696+Mdavid508@users.noreply.github.com>
2024-02-03 02:03:27 +0300

    Creation and changing of Navigation menus for all users

---
+++ lib/views/collectors/collector_navigation_menu.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CollectorNavigationMenu extends StatelessWidget {
+  const CollectorNavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
