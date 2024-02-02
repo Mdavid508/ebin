66cbf47df32e53964427adfc6c96034bf127d281
Mdavid508 <81154696+Mdavid508@users.noreply.github.com>
2024-02-03 02:01:28 +0300

    Creation of models to handle electronic items

---
+++ lib/models/category.dart
@@ -0,0 +1,10 @@
+class ItemCategory {
+  static const String _screensAndMonitors = 'Screens and Monitors';
+  static const String _itEquipments = 'It Equipments';
+  static const String _smallEquipments = 'Small Equipments';
+  static const String _others = 'Others';
+  static String get screensAndMonitors => _screensAndMonitors;
+  static String get itEquipments => _itEquipments;
+  static String get smallEquipments => _smallEquipments;
+  static String get others => _others;
+}
---
+++ lib/models/item_eol.dart
@@ -0,0 +1,14 @@
+class ItemEol {
+  final String itemName;
+  final String url;
+  final int eol;
+  final String category;
+
+//constructor of this class.
+  ItemEol({
+    required this.itemName,
+    required this.url,
+    required this.eol,
+    required this.category,
+  });
+}
