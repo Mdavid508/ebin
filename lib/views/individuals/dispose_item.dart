b12061ded2179ea7a495428c4f93aa67c6bf4be7
Mdavid508 <81154696+Mdavid508@users.noreply.github.com>
2024-02-22 09:39:38 -0600

    added a page to display a list of the items to be disposed

--- lib/views/individuals/dispose_item.dart
+++ lib/views/individuals/dispose_item.dart
@@ -1,163 +1,102 @@
 import 'package:ebin/Assets/Theme/custom_theme/text_theme.dart';
-import 'package:ebin/constants/colors.dart';
-import 'package:ebin/views/individuals/it_equipments.dart';
-import 'package:ebin/views/individuals/navigation_menu.dart';
+import 'package:ebin/controllers/collectionpoint_controller.dart';
+import 'package:ebin/controllers/items_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class DisposeItem extends StatelessWidget {
-  const DisposeItem({super.key});
+  DisposeItem({super.key});
+
+  final controllerItem = Get.put(ItemsController());
+  final controllerLocation = Get.put(CollectionPointController());
 
   @override
   Widget build(BuildContext context) {
     final textTheme = MyAppTextTheme.lightTheme;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dispose'),
+        title: const Text('Dispose Electronic Item'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              TextFormField(
+                controller: controllerLocation.controllerCollectionPointName,
+                readOnly: true,
+                decoration: const InputDecoration(
+                  labelText: 'Collection Point Name',
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: controllerItem.controllerItemName,
+                readOnly: true,
+                decoration: const InputDecoration(
+                  labelText: 'Item Name',
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: controllerItem.controllerBrand,
+                decoration: const InputDecoration(
+                  labelText: 'Brand Name',
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
               Text(
-                'Select the Category to Dispose',
-                style: textTheme.titleMedium,
+                'Upload a Minimum of 2 images',
+                style: textTheme.bodyLarge,
               ),
               const SizedBox(
-                height: 24,
+                height: 8,
               ),
-              const Column(
-                children: [SingleChildScrollView()],
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  padding: const EdgeInsets.fromLTRB(12, 12, 8, 12),
-                  width: 328,
-                  decoration: BoxDecoration(
-                    color: MyAppColors.surfaceLightColor,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+              Row(
+                children: [
+                  Wrap(
+                    alignment: WrapAlignment.start,
+                    spacing: 8,
                     children: [
-                      MyCategoryContainerBuilder(
-                          image:
-                              'https://d17kynu4zpq5hy.cloudfront.net/igi/ric/3tWdqRoMU63cuci1.standard',
-                          categoriesHeading: 'Screens and Monitors',
-                          categories: 'CRT monitors, Flat panel Display'),
+                      OutlinedButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(Icons.camera_alt_outlined),
+                        label: const Text('Take Photo'),
+                      ),
+                      OutlinedButton.icon(
+                        onPressed: () {},
+                        icon: const Icon(Icons.image_outlined),
+                        label: const Text(' Pick from Gallery'),
+                      ),
                     ],
                   ),
-                ),
+                ],
               ),
-              const SizedBox(
-                height: 16,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Get.to(MyItEquipmentsDisposeList());
-                },
-                child: Container(
-                  padding: const EdgeInsets.fromLTRB(12, 12, 8, 12),
-                  width: 328,
-                  decoration: BoxDecoration(
-                    color: MyAppColors.surfaceLightColor,
-                    borderRadius: BorderRadius.circular(10),
+              Row(
+                children: [
+                  Flexible(
+                    flex: 2,
+                    child: Image.asset(height: 150, 'name'),
                   ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MyCategoryContainerBuilder(
-                          image:
-                              'https://d17kynu4zpq5hy.cloudfront.net/igi/ric/4Tba1Ob6xOYGVFPr.standard',
-                          categoriesHeading: 'IT Equipments',
-                          categories:
-                              'Mobile Phones, SmartPhones, Computers, Laptops, Tablets, Fax machines, Routers, Printers and Scanners'),
-                    ],
+                  const SizedBox(
+                    width: 16,
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              GestureDetector(
-                child: Container(
-                  padding: const EdgeInsets.fromLTRB(12, 12, 8, 12),
-                  width: 328,
-                  decoration: BoxDecoration(
-                    color: MyAppColors.surfaceLightColor,
-                    borderRadius: BorderRadius.circular(10),
+                  Flexible(
+                    flex: 2,
+                    child: Image.asset(height: 150, 'name'),
                   ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MyCategoryContainerBuilder(
-                          image:
-                              'https://d17kynu4zpq5hy.cloudfront.net/igi/ric/joYPDKnMa6ndYOra.standard',
-                          categoriesHeading: 'Small Equipments',
-                          categories:
-                              'Microwaves, Toasters, Headphones, Speakers, DVD player, Hand mixer'),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              GestureDetector(
-                child: Container(
-                  padding: const EdgeInsets.fromLTRB(12, 12, 12, 12),
-                  width: 328,
-                  decoration: BoxDecoration(
-                    color: MyAppColors.surfaceLightColor,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MyCategoryContainerBuilder(
-                          image:
-                              'https://d17kynu4zpq5hy.cloudfront.net/igi/ric/kjwfrwPouDBA3Xh6.standard',
-                          categoriesHeading: 'Others',
-                          categories:
-                              'Refrigerators, Dish washing machines, Ovens, Washing Machines, Air Conditioning Equipments, Flourescent bulbs, Cameras'),
-                    ],
-                  ),
-                ),
+                ],
               ),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class CategoryBuilder extends StatelessWidget {
-  const CategoryBuilder({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(12, 12, 8, 12),
-      width: 328,
-      decoration: BoxDecoration(
-        color: MyAppColors.surfaceLightColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          MyCategoryContainerBuilder(
-              image:
-                  'https://d17kynu4zpq5hy.cloudfront.net/igi/ric/3tWdqRoMU63cuci1.standard',
-              categoriesHeading: 'Screens and Monitors',
-              categories: 'CRT monitors, Flat panel Display'),
-          SizedBox(
-            height: 8,
-          ),
-        ],
       ),
     );
   }
